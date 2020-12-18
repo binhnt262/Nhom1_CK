c50e0c3c988453f90f1980e4b6d6bb02f95f9c8c
binhnt262 <binhnguyen262109@gmail.com>
2020-12-18 09:42:39 +0700

    Frontend

---
+++ khachsan-react/src/Component/MenuGT/MenuGT.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MenuGT extends Component {
+    render() {
+        return (
+            <div className="col-lg-12">
+            <section class="page-section" id="services">
+                <div className="gt">
+                  <h2 class="section-heading text-uppercase">{this.props.gioithieu}</h2>
+                  <h3 class="section-subheading text-muted">{this.props.gioithieu2}</h3>
+                  </div>
+           </section>
+           </div>
+        );
+    }
+}
+
+export default MenuGT;
